2f62bff230cabb3d11f4528d0d79ebcb68124f7c
Victor Manuel Serrano Barrera <victor_manuel_serrano_apps1m1617@cev.com>
2017-03-23 12:18:27 +0100

    Acabo de implementar la funcion del boton compartir

--- app/src/main/java/com/example/alumnos/victormanuelserranobarrerahnd2/Fragments/DescripcionFragment.java
+++ app/src/main/java/com/example/alumnos/victormanuelserranobarrerahnd2/Fragments/DescripcionFragment.java
@@ -1,6 +1,7 @@
 package com.example.alumnos.victormanuelserranobarrerahnd2.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,8 +15,8 @@ import com.example.alumnos.victormanuelserranobarrerahnd2.R;
 
 
 
-public class DescripcionFragment extends Fragment {
-private TextView txtdescripcion;
+public class DescripcionFragment extends Fragment implements View.OnClickListener{
+    private TextView txtdescripcion;
     private Button compartir;
     private String descripcion;
 
@@ -30,12 +31,14 @@ private TextView txtdescripcion;
         View view = inflater.inflate(R.layout.fragment_descripcion, container, false);
 
         txtdescripcion = (TextView) view.findViewById(R.id.descripcion);
-        descripcion="Estamos ante el juego de naves mas revolucionario de los ultimos años, estamos ante un juego con grafica 4k para moviles de alto rendimiento cosa que no tiene ningun juego, no mas anuncios en los juegos nuestro parner sois vosotros disfrutar de este gran juego y ¡MAAAAAAATAAAAR MATAAAAAAR Y DESTRUIR!";
-        txtdescripcion.setText("Descripcion: "+descripcion);
+        compartir= (Button) view.findViewById(R.id.compartir);
+        descripcion = "Estamos ante el juego de naves mas revolucionario de los ultimos años, estamos ante un juego con grafica 4k para moviles de alto rendimiento cosa que no tiene ningun juego, no mas anuncios en los juegos nuestro parner sois vosotros disfrutar de este gran juego y ¡MAAAAAAATAAAAR MATAAAAAAR Y DESTRUIR!";
+        txtdescripcion.setText("Descripcion: " + descripcion);
+
+        compartir.setOnClickListener(this);
         // Inflate the layout for this fragment
         return view;
     }
-
 
 
     @Override
@@ -52,6 +55,19 @@ private TextView txtdescripcion;
 
     public static DescripcionFragment newInstance() {
         return new DescripcionFragment();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.compartir:
+                Intent intentCompartir = new Intent(Intent.ACTION_SEND);
+                intentCompartir.setType("text/plain");
+                intentCompartir.putExtra(Intent.EXTRA_TEXT, descripcion);
+                startActivity(Intent.createChooser(intentCompartir, "Compartir en..."));
+                break;
+
+        }
     }
 }
 
